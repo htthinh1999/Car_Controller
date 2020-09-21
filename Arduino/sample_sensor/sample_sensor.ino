e6c9921ae5000514baa46cbbed1bc1c819f09434
Huỳnh Tấn Thịnh <htthinh1999@gmail.com>
2020-09-21 15:16:50 +0700

    Init sample_sensor

---
+++ Arduino/sample_sensor/sample_sensor.ino
@@ -0,0 +1,15 @@
+int ir = 2;  // hong ngoai
+
+void setup() {
+  pinMode(ir, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int a = digitalRead(ir);
+  if(a==0){
+    Serial.println("Trang");
+  }else{
+    Serial.println("Den");
+  }
+}
